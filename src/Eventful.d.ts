4d45d49a113630966c2368ff7160203e7c01b0fc
yuanzhenkun <yuanzhenkun@baidu.com>
2021-08-10 18:23:57 +0800

    add .d.ts file.

--- src/Eventful.d.ts
+++
@@ -1,8 +0,0 @@
-export declare class Eventful {
-    private listeners;
-    addEventListener(eventName: string, listener: Function): void;
-    removeEventListener(eventName: string, listener: Function): boolean;
-    hasEventListener(eventName: string, listener: Function): boolean;
-    dispatchEvent(eventName: string, data: any): void;
-    dispose(): void;
-}
